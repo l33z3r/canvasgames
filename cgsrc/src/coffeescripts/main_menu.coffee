874eaba3fbdc0528958ae82ae3cc2fa871c9a384
Lee Farrell <lee.farrell@clavisinsight.com>
2014-12-11 17:06:18 +0000

    added menu

--- cgsrc/src/coffeescripts/main_menu.coffee
+++ cgsrc/src/coffeescripts/main_menu.coffee
@@ -8,21 +8,47 @@ define ["require", "game", "Settings"], (require, game, Settings) ->
 
 		render: (delta) ->
 			game.layer.clear Settings.appBGColor
-			game.layer.fillStyle("#000").font("64px Arial").fillText "Click The Mouse To Play!!!", 200, 200
+
+			textWidth = 200
+			x = Settings.gameWidth / 2.0 - textWidth
+			game.layer.fillStyle("#000").font("40px Arial").fillText "Please Choose A Game", x, 100
+
+			#bird game text boundry
+			textWidth = 250
+			textHeight = 80
+			x = Settings.gameWidth / 2.0 - textWidth / 2.0
+			y = 300
+			game.layer.fillStyle("#000").font("40px Arial").fillText "Pidgeon Dash", x, y
+			@game1Boundries = [x, y, textWidth, textHeight]
+
+			#bird game text boundry
+			textWidth = 220
+			x = Settings.gameWidth / 2.0 - textWidth / 2.0
+			game.layer.fillStyle("#000").font("40px Arial").fillText "Sketch Pad", x, 420
 
 		mousedown: (event) ->
+			#check which game was clicked
+			mouseX = event.x
+			mouseY = event.y
 
-			game.setState require("bird_game/game_screen")
+			#check bird game
+			x = @game1Boundries[0]
+			y = @game1Boundries[1]
+			width = @game1Boundries[2]
+			height = @game1Boundries[3]
 
-			#request fullscreen
-			doc = window.document
-			docEl = doc.documentElement
-			requestFullScreen = docEl.requestFullscreen or docEl.mozRequestFullScreen or docEl.webkitRequestFullScreen or docEl.msRequestFullscreen
-			cancelFullScreen = doc.exitFullscreen or doc.mozCancelFullScreen or doc.webkitExitFullscreen or doc.msExitFullscreen
-			if not doc.fullscreenElement and not doc.mozFullScreenElement and not doc.webkitFullscreenElement and not doc.msFullscreenElement
-				requestFullScreen.call docEl
-			else
-				cancelFullScreen.call doc
+			if mouseX > x and mouseX < x + width and mouseY > y and mouseY < y + height
+				game.setState require("bird_game/game_screen")
+
+				#request fullscreen
+				doc = window.document
+				docEl = doc.documentElement
+				requestFullScreen = docEl.requestFullscreen or docEl.mozRequestFullScreen or docEl.webkitRequestFullScreen or docEl.msRequestFullscreen
+				cancelFullScreen = doc.exitFullscreen or doc.mozCancelFullScreen or doc.webkitExitFullscreen or doc.msExitFullscreen
+				if not doc.fullscreenElement and not doc.mozFullScreenElement and not doc.webkitFullscreenElement and not doc.msFullscreenElement
+					requestFullScreen.call docEl
+				else
+					cancelFullScreen.call doc
 
 		mouseup: (event) ->
 
--- cgsrc/src/javascripts/main_menu.js
+++ cgsrc/src/javascripts/main_menu.js
@@ -5,20 +5,40 @@ define(["require", "game", "Settings"], function(require, game, Settings) {
     ready: function() {},
     step: function(delta) {},
     render: function(delta) {
+      var textHeight, textWidth, x, y;
       game.layer.clear(Settings.appBGColor);
-      return game.layer.fillStyle("#000").font("64px Arial").fillText("Click The Mouse To Play!!!", 200, 200);
+      textWidth = 200;
+      x = Settings.gameWidth / 2.0 - textWidth;
+      game.layer.fillStyle("#000").font("40px Arial").fillText("Please Choose A Game", x, 100);
+      textWidth = 250;
+      textHeight = 80;
+      x = Settings.gameWidth / 2.0 - textWidth / 2.0;
+      y = 300;
+      game.layer.fillStyle("#000").font("40px Arial").fillText("Pidgeon Dash", x, y);
+      this.game1Boundries = [x, y, textWidth, textHeight];
+      textWidth = 220;
+      x = Settings.gameWidth / 2.0 - textWidth / 2.0;
+      return game.layer.fillStyle("#000").font("40px Arial").fillText("Sketch Pad", x, 420);
     },
     mousedown: function(event) {
-      var cancelFullScreen, doc, docEl, requestFullScreen;
-      game.setState(require("bird_game/game_screen"));
-      doc = window.document;
-      docEl = doc.documentElement;
-      requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-      cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-      if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-        return requestFullScreen.call(docEl);
-      } else {
-        return cancelFullScreen.call(doc);
+      var cancelFullScreen, doc, docEl, height, mouseX, mouseY, requestFullScreen, width, x, y;
+      mouseX = event.x;
+      mouseY = event.y;
+      x = this.game1Boundries[0];
+      y = this.game1Boundries[1];
+      width = this.game1Boundries[2];
+      height = this.game1Boundries[3];
+      if (mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height) {
+        game.setState(require("bird_game/game_screen"));
+        doc = window.document;
+        docEl = doc.documentElement;
+        requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          return requestFullScreen.call(docEl);
+        } else {
+          return cancelFullScreen.call(doc);
+        }
       }
     },
     mouseup: function(event) {},
